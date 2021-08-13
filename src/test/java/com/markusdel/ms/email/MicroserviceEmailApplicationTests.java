f45a960d08c484627baa8ee3c462909bed34e640
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-08-12 22:31:07 -0300

    começando serviço de email

---
+++ src/test/java/com/markusdel/ms/email/MicroserviceEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markusdel.ms.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
