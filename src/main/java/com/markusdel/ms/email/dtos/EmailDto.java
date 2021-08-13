e25e72553767b713f02162b9c41a8cd8d2f9ad0a
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-08-12 22:42:48 -0300

    criando dto com validações

---
+++ src/main/java/com/markusdel/ms/email/dtos/EmailDto.java
@@ -0,0 +1,28 @@
+package com.markusdel.ms.email.dtos;
+
+import lombok.Data;
+import lombok.NonNull;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class EmailDto {
+
+    @NotBlank
+    private String ownerRef;
+
+    @Email
+    @NotBlank
+    private String emailFrom;
+
+    @Email
+    @NotBlank
+    private String emailTo;
+
+    @NotBlank
+    private String subject;
+
+    @NotBlank
+    private String text;
+}
